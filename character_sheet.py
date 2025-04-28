0717f3c79b60d91fefcc6c16da64eb9a5a3ba708
SamBrudell <brudellsam@gmail.com>
2025-04-28 22:02:48 +0100

    Merge branch 'main' of https://github.com/SamBrudell/python-practice

--- character_sheet.py
+++ character_sheet.py
@@ -1,13 +1,24 @@
 # variables practice
 # character sheet
-
+import time
 name = "Zara" # this is a string
 age = 20 # this is an integer
 magic_power_level = 3.5 # this is a float
 is_hero = True # this is a boolean
+favoruite_color = "green"
+spell_list = ["magic missile", "shield", "heal"] # this is a list
+weapon = "staff"
 
 profile = name, age, magic_power_level, is_hero # this is a set
 
 print(profile)
 
+time.sleep(2)
+
 print("Meet " + name + ", a " + str(age) + " year old hero with a magic power level of " + str(magic_power_level) + "!")
+
+time.sleep(2)
+
+print("Her adventure is about to begin...")
+
+time.sleep(2)
