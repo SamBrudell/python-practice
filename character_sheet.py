c7ef2b4e7031f6477cd789b5014acbac9c4b4ab9
SamBrudell <brudellsam@gmail.com>
2025-04-28 20:56:06 +0100

    Day 2: Created character sheet with variable sna ddata types - strings, integers, floats and booleans.

---
+++ character_sheet.py
@@ -0,0 +1,13 @@
+# variables practice
+# character sheet
+
+name = "Zara" # this is a string
+age = 20 # this is an integer
+magic_power_level = 3.5 # this is a float
+is_hero = True # this is a boolean
+
+profile = name, age, magic_power_level, is_hero # this is a set
+
+print(profile)
+
+print("Meet " + name + ", a " + str(age) + " year old hero with a magic power level of " + str(magic_power_level) + "!")
